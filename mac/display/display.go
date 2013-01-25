76317025bd2cad214c7e01b6661ba70213dd5a8a
youpy <youpy@buycheapviagraonlinenow.com>
2013-01-26 01:32:32 +0900

    initial commit

---
+++ mac/display/display.go
@@ -0,0 +1,39 @@
+package mac
+
+/*
+#cgo LDFLAGS: -framework IOKit -framework ApplicationServices -fconstant-cfstrings
+#include <CoreFoundation/CoreFoundation.h>
+#include <ApplicationServices/ApplicationServices.h>
+#include <IOKit/graphics/IOGraphicsLib.h>
+*/
+import "C"
+
+type Display struct {
+	id int
+}
+
+func MainDisplay() *Display {
+	display := Display{int(C.CGMainDisplayID())}
+
+	return &display
+}
+
+func (display Display) Brightness() float64 {
+	value := C.float(0.0)
+
+	C.IODisplayGetFloatParameter(
+		C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
+		C.kNilOptions,
+		C.CFStringCreateWithCString(nil, C.CString(C.kIODisplayLinearBrightnessKey), C.kCFStringEncodingMacRoman),
+		&value)
+
+	return float64(value)
+}
+
+func (display Display) SetBrightness(value float64) {
+	C.IODisplaySetFloatParameter(
+		C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
+		C.kNilOptions,
+		C.CFStringCreateWithCString(nil, C.CString(C.kIODisplayLinearBrightnessKey), C.kCFStringEncodingMacRoman),
+		C.float(value))
+}
---
+++ mac/display/display_test.go
@@ -0,0 +1,24 @@
+package mac
+
+import "testing"
+
+func TestGetDisplayBrightness(t *testing.T) {
+
+	display := MainDisplay()
+	brightness := display.Brightness()
+
+	if !(brightness >= 0.0 && brightness <= 1.0) {
+		t.Fatalf("brightness is invalid")
+	}
+}
+
+func TestSetDisplayBrightness(t *testing.T) {
+	display := MainDisplay()
+	display.SetBrightness(1.0)
+
+	brightness := display.Brightness()
+
+	if brightness != 1.0 {
+		t.Fatalf("brightness is invalid: %f", brightness)
+	}
+}
