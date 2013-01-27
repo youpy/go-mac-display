791fde74a1d868a6eb6a44fb0699ae704f4280e5
youpy <youpy@buycheapviagraonlinenow.com>
2013-01-27 12:22:38 +0900

    free C string

--- display.go
+++ display.go
@@ -5,8 +5,10 @@ package display
 #include <CoreFoundation/CoreFoundation.h>
 #include <ApplicationServices/ApplicationServices.h>
 #include <IOKit/graphics/IOGraphicsLib.h>
+#include <stdlib.h>
 */
 import "C"
+import "unsafe"
 
 type Display struct {
 	id int
@@ -18,22 +20,35 @@ func MainDisplay() *Display {
 	return &display
 }
 
+func DoWithBrightness(callback func(key *C.char)) {
+	key := C.CString(C.kIODisplayLinearBrightnessKey)
+
+	callback(key)
+
+	// http://code.google.com/p/go-wiki/wiki/cgo#Go_strings_and_C_strings
+	defer C.free(unsafe.Pointer(key))
+}
+
 func (display Display) Brightness() float64 {
 	value := C.float(0.0)
 
-	C.IODisplayGetFloatParameter(
-		C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
-		C.kNilOptions,
-		C.CFStringCreateWithCString(nil, C.CString(C.kIODisplayLinearBrightnessKey), C.kCFStringEncodingMacRoman),
-		&value)
+	DoWithBrightness(func(key *C.char) {
+		C.IODisplayGetFloatParameter(
+			C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
+			C.kNilOptions,
+			C.CFStringCreateWithCString(nil, key, C.kCFStringEncodingMacRoman),
+			&value)
+	})
 
 	return float64(value)
 }
 
 func (display Display) SetBrightness(value float64) {
-	C.IODisplaySetFloatParameter(
-		C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
-		C.kNilOptions,
-		C.CFStringCreateWithCString(nil, C.CString(C.kIODisplayLinearBrightnessKey), C.kCFStringEncodingMacRoman),
-		C.float(value))
+	DoWithBrightness(func(key *C.char) {
+		C.IODisplaySetFloatParameter(
+			C.CGDisplayIOServicePort(C.CGDirectDisplayID(display.id)),
+			C.kNilOptions,
+			C.CFStringCreateWithCString(nil, key, C.kCFStringEncodingMacRoman),
+			C.float(value))
+	})
 }
